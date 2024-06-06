38abd96f6132104d9e166a8f5c6dccc8f63168eb
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-05 20:45:23 -0500

    Add Initial class commented

--- SolidPrinciples.SingleResponsability/Book.cs
+++ SolidPrinciples.SingleResponsability/Book.cs
@@ -1,5 +1,33 @@
 ﻿namespace SolidPrinciples.SingleResponsability
 {
+    //// Class with 3 resposabilities:
+    // 1. Store information about a book.
+    // 2. Save the book.
+    // 3. Manage printing of book details.
+
+
+    // Store information about a book //
+    //public class Book
+    //{
+    //    public string Title { get; set; }
+    //    public string Author { get; set; }
+    //    public string ISBN { get; set; }
+
+    // Save the book //
+    //    public void Save()
+    //    {
+    //        // Código para guardar el libro en la base de datos
+    //    }
+
+    // Manage printing of book details //
+    //    public void Print()
+    //    {
+    //        // Código para imprimir los detalles del libro
+    //    }
+    //}
+
+
+
     public class Book
     {
         public string Title { get; set; }
