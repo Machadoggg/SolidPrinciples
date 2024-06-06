ea1f36c70d8850db7f7b539ffcfa91bade558d5a
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-05 20:32:27 -0500

    Separation of responsibilities in 3 independent classes

--- SolidPrinciples.SingleResponsability/Book.cs
+++ SolidPrinciples.SingleResponsability/Book.cs
@@ -5,15 +5,22 @@
         public string Title { get; set; }
         public string Author { get; set; }
         public string ISBN { get; set; }
+    }
 
+    public class BookRepository
+    {
         public void Save()
         {
             // Code for save the book in DB
         }
+    }
 
+    public class BookPrinter
+    {
         public void Print()
         {
             // Code for print book details
         }
     }
 }
+
