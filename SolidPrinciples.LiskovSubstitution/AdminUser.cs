a1090b2a3f2a31f12e4449f40fa01e1caf60b1d7
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-16 13:38:19 -0500

    Derived class AdminUser, override AccessAdminPanel method

---
+++ SolidPrinciples.LiskovSubstitution/AdminUser.cs
@@ -0,0 +1,10 @@
+﻿namespace SolidPrinciples.LiskovSubstitution
+{
+    public class AdminUser : User
+    {
+        public override void AccessAdminPanel()
+        {
+            Console.WriteLine("Admin user accessing admin panel");
+        }
+    }
+}
