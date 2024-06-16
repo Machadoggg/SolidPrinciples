610d3eafb92facf2433881f5b470695df750950f
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-16 15:15:47 -0500

    Using specific classes in main

--- SolidPrinciples.InterfaceSegregation/Program.cs
+++ SolidPrinciples.InterfaceSegregation/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using SolidPrinciples.InterfaceSegregation;
+
+IBlackAndWhitePrinter basiPrinter = new BasicPrinter();
+IBlackAndWhitePrinter advancedPrinterBlackAndWhite = new AdvancedPrinter();
+IColorPrinter advancedPrintColor = new AdvancedPrinter();
+IScanner advancedPrinterScanner = new AdvancedPrinter();
+
+
+Console.WriteLine("Using Basic printer: ");
+basiPrinter.PrintBlackAndWhite("Basic document");
+
+Console.WriteLine("\nUsing advanced printer for black and white: ");
+advancedPrinterBlackAndWhite.PrintBlackAndWhite("Advanced document black and white");
+
+Console.WriteLine("\nUsing advanced printer for color: ");
+advancedPrintColor.PrintColor("Advanced document color");
+
+Console.WriteLine("\nUsing advanced printer for scanning: ");
+advancedPrinterScanner.ScanDocument("Advanced document scan");
