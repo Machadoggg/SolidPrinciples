af6e8ccf92d084269c9b9e94769a48ba312cd1ac
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-16 01:12:10 -0500

    Specify class Rectaangle, implement IShape

---
+++ SolidPrinciples.OpenClose/Rectangle.cs
@@ -0,0 +1,19 @@
+﻿namespace SolidPrinciples.OpenClose
+{
+    public class Rectangle : IShape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public double CalculateArea()
+        {
+            return Width * Height;
+        }
+    }
+}
