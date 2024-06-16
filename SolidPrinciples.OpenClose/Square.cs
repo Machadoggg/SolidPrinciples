aff08d17325693b0f6cad281d34470e06d559438
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-16 13:21:18 -0500

    Ass Square class, implement IShape

---
+++ SolidPrinciples.OpenClose/Square.cs
@@ -0,0 +1,18 @@
+﻿namespace SolidPrinciples.OpenClose
+{
+    public class Square : IShape
+    {
+        public double Side { get; set; }
+
+        public Square(double side)
+        {
+            Side = side;
+        }
+
+
+        public double CalculateArea()
+        {
+            return Side * Side;
+        }
+    }
+}
