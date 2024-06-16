e4186628db4d23c9912d5a8a41503517d9c874e5
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-16 14:43:23 -0500

    Initial large interface that brings together all the functionalities

---
+++ SolidPrinciples.InterfaceSegregation/IPrinter.cs
@@ -0,0 +1,9 @@
+﻿namespace SolidPrinciples.InterfaceSegregation
+{
+    public interface IPrinter
+    {
+        void PrintBlackAndWhite(string document);
+        void PrintColor(string document);
+        void ScanDocument(string document);
+    }
+}
