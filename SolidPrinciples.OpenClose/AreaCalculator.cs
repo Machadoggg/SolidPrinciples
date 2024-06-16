3433498bbced0d085c8db15068dcf75ff5a86a47
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-16 01:16:16 -0500

    AreaCalculator class to work IShape

---
+++ SolidPrinciples.OpenClose/AreaCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace SolidPrinciples.OpenClose
+{
+    public class AreaCalculator
+    {
+        public double CalculateArea(IShape shape) 
+        {
+            return shape.CalculateArea();
+        }
+    }
+}
