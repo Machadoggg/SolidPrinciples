e5fad4e44e27ef2f82a792b7c87d5e78564637ea
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-16 13:25:10 -0500

    Add derived class Square in main

--- SolidPrinciples.OpenClose/Program.cs
+++ SolidPrinciples.OpenClose/Program.cs
@@ -4,6 +4,7 @@ using SolidPrinciples.OpenClose;
 
 IShape circle = new Circle(5);
 IShape rectangle = new Rectangle(3, 4);
+IShape square = new Square(6);
 
 
 AreaCalculator calculator = new AreaCalculator();
@@ -11,3 +12,4 @@ AreaCalculator calculator = new AreaCalculator();
 
 Console.WriteLine($"Circle area: {calculator.CalculateArea(circle)}");
 Console.WriteLine($"Rectangle area: {calculator.CalculateArea(rectangle)}");
+Console.WriteLine($"Square area: {calculator.CalculateArea(square)}");
