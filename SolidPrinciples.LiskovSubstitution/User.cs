8e1516fb1d16265cce951d1673c951821bf96795
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-16 13:34:44 -0500

    Base class User

---
+++ SolidPrinciples.LiskovSubstitution/User.cs
@@ -0,0 +1,16 @@
+﻿namespace SolidPrinciples.LiskovSubstitution
+{
+    public class User
+    {
+
+        public virtual void AccessDashboard()
+        {
+            Console.WriteLine("User accessing dashboard");
+        }
+
+        public virtual void AccessAdminPanel()
+        {
+            throw new InvalidOperationException("User does not have access to the admin panel");
+        }
+    }
+}
