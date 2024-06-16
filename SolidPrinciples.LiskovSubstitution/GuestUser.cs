755367287a85e0ebeec4cea7b2390beb719d37d2
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-16 13:43:11 -0500

    Derived class GuestUser , override AccessDashboard and AccessAdminPanel

---
+++ SolidPrinciples.LiskovSubstitution/GuestUser.cs
@@ -0,0 +1,15 @@
+﻿namespace SolidPrinciples.LiskovSubstitution
+{
+    public class GuestUser : User
+    {
+        public override void AccessDashboard()
+        {
+            Console.WriteLine("Guest user accessing limited dashboard");
+        }
+
+        public override void AccessAdminPanel()
+        {
+            throw new InvalidOperationException("Guest user does not have access to the admin panel");
+        }
+    }
+}
