652bd64d9c6028c770a0297b9d5a257bd1ebefb4
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-16 14:54:27 -0500

    Add Basic black and white printer

---
+++ SolidPrinciples.InterfaceSegregation/BasicPrinter.cs
@@ -0,0 +1,10 @@
+﻿namespace SolidPrinciples.InterfaceSegregation
+{
+    public class BasicPrinter : IBlackAndWhitePrinter
+    {
+        public void PrintBlackAndWhite(string document)
+        {
+            Console.WriteLine($"Printing document in black and white: {document}");
+        }
+    }
+}
