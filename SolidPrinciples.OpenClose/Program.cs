ab4803ae321f725a2f3314d0a8af5a5fe5e3cb29
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-16 01:22:51 -0500

    Using pinciple in main

--- SolidPrinciples.OpenClose/Program.cs
+++ SolidPrinciples.OpenClose/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using SolidPrinciples.OpenClose;
+
+IShape circle = new Circle(5);
+IShape rectangle = new Rectangle(3, 4);
+
+
+AreaCalculator calculator = new AreaCalculator();
+
+
+Console.WriteLine($"Circle area: {calculator.CalculateArea(circle)}");
+Console.WriteLine($"Rectangle area: {calculator.CalculateArea(rectangle)}");
