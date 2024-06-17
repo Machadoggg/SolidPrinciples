6c86d713c5b799175fc98e6e8b882497dde76dbe
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-17 08:03:37 -0500

    SmsSender clas implement IMessageSender

---
+++ SolidPrinciples.DependencyInversion/SmsSender.cs
@@ -0,0 +1,10 @@
+﻿namespace SolidPrinciples.DependencyInversion
+{
+    public class SmsSender : IMessageSender
+    {
+        public void SendMessage(string message)
+        {
+            Console.WriteLine($"Send SMS: {message}");
+        }
+    }
+}
