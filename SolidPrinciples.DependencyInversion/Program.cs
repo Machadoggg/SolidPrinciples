dfac4af733eca67550f8f135084e1a12b2b51b51
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-17 08:28:58 -0500

    Allows changing implementations without modifying the code Main

--- SolidPrinciples.DependencyInversion/Program.cs
+++ SolidPrinciples.DependencyInversion/Program.cs
@@ -1,13 +1,27 @@
 ﻿
 
 // use Email sender
+//using SolidPrinciples.DependencyInversion;
+
+//IMessageSender emailSender = new EmailSender();
+//Notification emailNotification = new Notification(emailSender);
+//emailNotification.Send("Hello via Email!");
+
+//// use SMS sender
+//IMessageSender smsSender = new SmsSender();
+//Notification smsNotification = new Notification(smsSender);
+//smsNotification.Send("Hello via SMS");
+
+
+// Configure container Dependency Inversion (DI)
+using Microsoft.Extensions.DependencyInjection;
 using SolidPrinciples.DependencyInversion;
 
-IMessageSender emailSender = new EmailSender();
-Notification emailNotification = new Notification(emailSender);
-emailNotification.Send("Hello via Email!");
+var serviceProvider = new ServiceCollection()
+    .AddTransient<IMessageSender, EmailSender>() // Cambia a SmsSender según sea necesario
+    .BuildServiceProvider();
 
-// use SMS sender
-IMessageSender smsSender = new SmsSender();
-Notification smsNotification = new Notification(smsSender);
-smsNotification.Send("Hello via SMS");
+// Resolve dependency
+var messageSender = serviceProvider.GetService<IMessageSender>();
+var notification = new Notification(messageSender);
+notification.Send("Hello via Email or SMS depending on DI configuration!");
