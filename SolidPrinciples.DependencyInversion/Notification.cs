cdaabe3462faf73e42a25ccd83d867cba17fb614
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-17 08:07:53 -0500

    Created High Level Class Notification for use abstraction

---
+++ SolidPrinciples.DependencyInversion/Notification.cs
@@ -0,0 +1,18 @@
+﻿namespace SolidPrinciples.DependencyInversion
+{
+    public class Notification
+    {
+        private readonly IMessageSender _messageSender;
+
+        public Notification(IMessageSender messageSender)
+        {
+            _messageSender = messageSender;
+        }
+
+
+        public void Send(string message)
+        {
+            _messageSender.SendMessage(message);
+        }
+    }
+}
