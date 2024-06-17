f97888fa9be2c713343d8adc3222edc971784e5d
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-17 08:15:40 -0500

    Configure dependency in main

--- SolidPrinciples.DependencyInversion/Program.cs
+++ SolidPrinciples.DependencyInversion/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+// use Email sender
+using SolidPrinciples.DependencyInversion;
+
+IMessageSender emailSender = new EmailSender();
+Notification emailNotification = new Notification(emailSender);
+emailNotification.Send("Hello via Email!");
+
+// use SMS sender
+IMessageSender smsSender = new SmsSender();
+Notification smsNotification = new Notification(smsSender);
+smsNotification.Send("Hello via SMS");
