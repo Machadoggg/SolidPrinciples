fd630cddc2727aaa1e22f7f08c9d5ec163b5caf1
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-17 08:01:03 -0500

    EmailSender class implement IMessageSender

---
+++ SolidPrinciples.DependencyInversion/EmailSender.cs
@@ -0,0 +1,10 @@
+﻿namespace SolidPrinciples.DependencyInversion
+{
+    public class EmailSender : IMessageSender
+    {
+        public void SendMessage(string message)
+        {
+            Console.WriteLine($"Send Email: {message}");
+        }
+    }
+}
